1f0a500d9aa1f391e95caa8282f1b2cf97e9fc12
ChamomileTommy <chamomiletommy@gmail.com>
2023-11-06 18:58:16 +0900

    install router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
 import "./globals.css";
 
 const App = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello snapgram!
-        </h1>
+        <main className="flex h-screen">
+            <Routes>
+                {/* public routes */}
+                <Route path="/sign-in" element={<SigninForm />} />
+                {/* private routes */}
+                <Route index element={<Home />} />
+            </Routes>
+        </main>
     )
 }
 
