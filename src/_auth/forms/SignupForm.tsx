b0b908e6cbb4a09999638f15f9aa001f89d5beb7
ChamomileTommy <chamomiletommy@gmail.com>
2023-11-07 21:40:24 +0900

    create sign-up form layout

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,6 +1,7 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Button } from "@/components/ui/button"
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 import {
     Form,
     FormControl,
@@ -11,46 +12,108 @@ import {
     FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import * as z from "zod"
+import { SignupValidation } from "@/lib/validation/index";
+import { z } from "zod"
+import Loader from "@/components/shared/Loader";
+import * as React from "react";
 
-const formSchema = z.object({
-    username: z.string().min(2).max(50),
-})
 const SignupForm = () => {
+    const isLoading = false;
+
     // 1. Define your form.
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const form = useForm<z.infer<typeof SignupValidation>>({
+        resolver: zodResolver(SignupValidation),
         defaultValues: {
+            name: "",
             username: "",
+            email: "",
+            password: "",
         },
     })
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+    async function onSubmit(values: z.infer<typeof SignupValidation>) {
+        // const newUser = await createUserAccount(values);
     }
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <FormField
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Username</FormLabel>
-                            <FormControl>
-                                <Input placeholder="shadcn" {...field} />
-                            </FormControl>
-                            <FormDescription>
-                                This is your public display name.
-                </FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit">Submit</Button>
-            </form>
+            <div className="sm:w-420 flex-center flex-col">
+                <img src="/assets/images/logo.svg" alt="logo" />
+
+                <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+                    Create a new account
+                </h2>
+                <p className="text-light-3 small-medium md:base-regular mt-2">
+                    To use snapgram, Please enter your details
+                </p>
+
+                <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel className="shad-form_label">name</FormLabel>
+                                <FormControl>
+                                    <Input type="text" className="shad-input" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel className="shad-form_label">Username</FormLabel>
+                                <FormControl>
+                                    <Input type="text" className="shad-input" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel className="shad-form_label">Email</FormLabel>
+                                <FormControl>
+                                    <Input type="text" className="shad-input" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel className="shad-form_label">Password</FormLabel>
+                                <FormControl>
+                                    <Input type="text" className="shad-input" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit" className="shad-button_primary">
+                        {isLoading ? (<div className="flex-center gap-2">
+                            <Loader />Loading...
+                        </div>) : ("Sign up")}
+                    </Button>
+                    <p className="text-small-regular text-light-2 text-center mt-2">
+                        Already have an account?
+                        <Link
+                            to="/sign-in"
+                            className="text-primary-500 text-small-semibold ml-1">
+                            Log in
+                        </Link>
+                    </p>
+                </form>
+            </div>
         </Form>
     )
 }
